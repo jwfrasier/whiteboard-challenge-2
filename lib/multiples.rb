4c7466ea9cead59c51650b314036559fcdb81f03
Joseph Fraiser <jfrasier@SF-US04880-MBP13.local>
2018-10-23 11:30:35 -0500

    solution not in oo

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+
+numbers = (1..999).to_a
+result = 0
+numbers.each do |number|
+  if number % 3 == 0 or number % 5 == 0
+    result += number
+  end
+end
+print result
